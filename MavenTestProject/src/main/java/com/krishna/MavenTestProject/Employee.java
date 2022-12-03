2fe342370e824d7a69d7beb05994f9678fec429b
91889 <91889@host.docker.internal>
2022-12-03 19:55:53 +0000

    Change Package

--- MavenTestProject/src/main/java/com/krishna/MavenTestProject/Employee.java
+++ MavenTestProject/src/main/java/com/krishna/MavenTestProject/Employee.java
@@ -6,41 +6,51 @@ public class Employee {
 
 	private int empNo;
 	private String empName;
-	
 	@Autowired
 	private Address address;
-	
+
+	public Employee() {
+	}
+
+	public Employee(int empNo, String empName, Address address) {
+		this.empNo = empNo;
+		this.empName = empName;
+		this.address = address;
+	}
+
 	public int getEmpNo() {
 		return empNo;
 	}
+
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
+
 	public String getEmpName() {
 		return empName;
 	}
+
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
+
 	public Employee(int empNo, String empName) {
 		this.empNo = empNo;
 		this.empName = empName;
 	}
-	public Employee() {
-	}
-	@Override
-	public String toString() {
-		
-		String emp= "Employee [empNo=" + empNo + ", empName=" + empName + "]";
-		String address= getAddress().toString();
-		return emp+"\n"+address;
-	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
-	
+
+	public String toString() {
+		String emp = "Employee [empNo=" + empNo + ", empName=" + empName + "]";
+		String address = getAddress().toString();
+		return emp + "\n" + address;
+	}
+
 }
