70012c487c91b19609f4cb058fa5f83cdac10f07
91889 <91889@host.docker.internal>
2022-12-03 14:14:32 +0000

    Spring Project Initial Commit

---
+++ SpringTestsProj/src/main/java/com/krishna/spring/SpringTestsProjApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestsProjApplication.class, args);
+	}
+
+}
---
+++ SpringTestsProj/src/test/java/com/krishna/spring/SpringTestsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
